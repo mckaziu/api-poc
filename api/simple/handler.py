7d03de6553eb60daf2d309af1b491cd7c5ebf423
Andrzej <andrzej@debian>
2021-10-25 13:37:48 +0200

    simple handler: extracted validation and responses into decorators

--- api/simple/handler.py
+++ api/simple/handler.py
@@ -1,6 +1,7 @@
 from http.server import SimpleHTTPRequestHandler
 import json
 import model.test_data as td
+import functools
 
 
 class MyHandler(SimpleHTTPRequestHandler):
@@ -41,70 +42,61 @@ class MyHandler(SimpleHTTPRequestHandler):
         id = parsed[3] if len(parsed) == 4 else None
         return id
 
-    # TODO: extract decorator (beginning and end)
+    def validate_request(handler):
+        @functools.wraps(handler)
+        def wrapper(self):
+            if not self._validate():
+                self._handle_wrong_endpoint()
+                return
+            return handler(self)
+        return wrapper
+
+    def write_response(handler):
+        @functools.wraps(handler)
+        def wrapper(self):
+            output = handler(self)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/json')
+            self.end_headers()
+            self.wfile.write(output.encode('utf-8'))
+        return wrapper
+
+    def standard_response(method):
+        def inner(handler):
+            @functools.wraps(handler)
+            def wrapper(self):
+                handler(self)
+                output = {'status': 'OK', 'result': method}
+                output_data = json.dumps(output)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/json')
+                self.end_headers()
+                self.wfile.write(output_data.encode('utf-8'))
+            return wrapper
+        return inner
+
+    @standard_response('POST')
+    @validate_request
     def do_POST(self):
-        if not self._validate():
-            self._handle_wrong_endpoint()
-            return
-
         payload = self._extract_payload()
-
         self.operations.create(payload[0], payload[1])
 
-        output_data = {'status': 'OK', 'result': 'POST'}
-        output_json = json.dumps(output_data)
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        self.wfile.write(output_json.encode('utf-8'))
-
+    @write_response
+    @validate_request
     def do_GET(self):
-        if not self._validate():
-            self._handle_wrong_endpoint()
-            return
-
         id = self._extract_id()
-
         result = self.operations.read(id)
-        output_json = json.dumps(result)
+        return json.dumps(result)
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        self.wfile.write(output_json.encode('utf-8'))
-
+    @standard_response('PUT')
+    @validate_request
     def do_PUT(self):
-        if not self._validate():
-            self._handle_wrong_endpoint()
-            return
-
         id = self._extract_id()
-
         input_data = self._extract_payload()
         self.operations.update(id, input_data[0], input_data[1])
 
-        output_data = {'status': 'OK', 'result': 'PUT'}
-        output_json = json.dumps(output_data)
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        self.wfile.write(output_json.encode('utf-8'))
-
+    @standard_response('DELETE')
+    @validate_request
     def do_DELETE(self):
-        if not self._validate():
-            self._handle_wrong_endpoint()
-            return
-
         id = self._extract_id()
-
         self.operations.delete(id)
-
-        output_data = {'status': 'OK', 'result': 'DELETE'}
-        output_json = json.dumps(output_data)
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        self.wfile.write(output_json.encode('utf-8'))
